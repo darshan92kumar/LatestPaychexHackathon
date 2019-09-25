b73a9d0b73344990385e200e3d0fdaa675930c79
Darshan Kumar <darshan92kumar@gmail.com>
2019-09-25 23:47:15 +0530

    Email improvements

--- src/main/java/com/paychex/gitplease/hackathon/serviceImpl/EmailServiceImpl.java
+++ src/main/java/com/paychex/gitplease/hackathon/serviceImpl/EmailServiceImpl.java
@@ -8,11 +8,17 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import com.paychex.gitplease.hackathon.config.TemplateConfig;
 import com.paychex.gitplease.hackathon.models.Quote;
 import com.paychex.gitplease.hackathon.service.EmailService;
 
 @Service
 public class EmailServiceImpl implements EmailService {
+	
+	
+	public static final String name = "<NAME>";
+	public static final String price = "<PRICE>";
+	public static final String service = "<SERVICE>";
 	
 	@Autowired
     private JavaMailSender sender;
@@ -20,17 +26,24 @@ public class EmailServiceImpl implements EmailService {
 	@Override
 	public void sendEmail(Quote quote) throws MessagingException {
 		
-		 MimeMessage message = sender.createMimeMessage();
+		 	MimeMessage message = sender.createMimeMessage();
 	        MimeMessageHelper helper = new MimeMessageHelper(message);
 	         
 	        helper.setTo(quote.getEmail());
-	        helper.setText("Our quoted price is 80,000$ for your requested service: "+quote.getService()); // This will be dynamic in future
+	        helper.setText(setText(quote),true); // This will be dynamic in future
 	        helper.setSubject("Quote from paychex for your requested service "+quote.getService());
 	        System.out.println("Sending email to "+quote.getFirstName()+","+quote.getLastName());
 	         
 	        sender.send(message);
 	        System.out.println("Email Sent !");	
 
+	}
+
+	private String setText(Quote quote) {
+		
+		String emailTemplate = TemplateConfig.getTemplate();
+		
+		return emailTemplate.replace(name, quote.getFirstName()+" "+quote.getLastName()).replace(price, "50,000").replace(service, quote.getService());
 	}
 
 }
